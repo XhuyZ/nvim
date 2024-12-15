93e2bfc50e954bdf47bd63a37ae2b31147d33b4a
Huykocony35 <huylovengoc35@gmail.com>
2024-12-16 04:34:31 +0700

    add bufferline plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,4 @@
-return {
+return{
 	{
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -7,6 +7,8 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+   vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>') 
+   
 }
 }
