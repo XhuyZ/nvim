455d86bfd87e7c0764fd513babd0be123cf30c34
Huykocony35 <huylovengoc35@gmail.com>
2024-12-16 00:56:52 +0700

    lua config

---
+++ lua/plugins/themery.lua
@@ -0,0 +1,13 @@
+return {
+    "zaldih/themery.nvim",
+    lazy = false,
+    config = function()
+      require("themery").setup({
+        -- add the config here
+	themes = {"catppuccin","tokyonight","cyberdream","bluloco","solarized-osaka"}, -- Your list of installed colorschemes.
+  livePreview = true, -- Apply theme while picking. Default to true.
+      })
+      vim.keymap.set("n", "<leader>uC","<cmd>Themery<cr>")
+    end
+  }
+  
