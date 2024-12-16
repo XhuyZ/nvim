eb42a6d1294b3bf55ec21a4a48861fc8d4d96dfa
Huykocony35 <huylovengoc35@gmail.com>
2024-12-16 23:37:48 +0700

    update bufferline

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,30 @@
 return{
 	{
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-   vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>') 
+   -- vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>') 
+   keys = {
+		{
+			"<leader>E",
+			"<cmd>Neotree toggle<cr>",
+			desc = "File Explorer",
+		},
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({
+					toggle = true,
+					reveal_force_cwd = true,
+				})
+			end,
+			desc = "File Explorer",
+		},
+	},
    
-}
+},
 }
