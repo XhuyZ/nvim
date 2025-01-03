22705f50ca5f3ca72430d544262b9851afc0c1d8
Huykocony35 <huylovengoc35@gmail.com>
2025-01-03 15:01:01 +0700

    add copilot plugin

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,15 +1,14 @@
 return {
-	-- Lazy
 	{
 		"jackMort/ChatGPT.nvim",
-		event = "VeryLazy",
 		config = function()
-			require("chatgpt").setup()
+			require("chatgpt").setup({
+				api_key_cmd = "echo $OPENAI_API_KEY",
+			})
 		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
-			"folke/trouble.nvim", -- optional
 			"nvim-telescope/telescope.nvim",
 		},
 	},
