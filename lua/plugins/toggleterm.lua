1eb09e30cfc9e761decd491042947d66f61917e2
XhuyZ <huylovengoc35@gmail.com>
2025-02-26 13:18:21 +0700

    fail

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*", -- or specify a version like "v2.0.0"
-		opts = {
-			direction = "float",
-			open_mapping = [[<c-\>]],
-			shell = "nu.exe",
-		},
-		config = true,
-	},
+  -- {
+  -- 	"akinsho/toggleterm.nvim",
+  -- 	version = "*", -- or specify a version like "v2.0.0"
+  -- 	opts = {
+  -- 		direction = "float",
+  -- 		open_mapping = [[<c-\>]],
+  -- 		shell = "nu.exe",
+  -- 	},
+  -- 	config = true,
+  -- },
   {
     "akinsho/toggleterm.nvim",
     version = "*", -- or specify a version like "v2.0.0"
