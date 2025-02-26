dd083396f407e56e2c63726d680a7d37a172c60c
XhuyZ <huylovengoc35@gmail.com>
2025-02-26 12:57:01 +0700

    repair themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+
+}
