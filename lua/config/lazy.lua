663bbd2360b8b68d46e41b599a0468c8b68f507a
XhuyZ <huylovengoc35@gmail.com>
2025-02-27 15:14:13 +0700

    update nvim config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -20,14 +20,14 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
+  spec = {
+    "akinsho/toggleterm.nvim",
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		"akinsho/toggleterm.nvim",
-		{ import = "plugins" },
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "catppuccin" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "catppuccin" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -13,6 +13,9 @@ return {
     lazy = false,
     priority = 1000,
   },
+  {
+    "navarasu/onedark.nvim"
+  },
 
 
 }
