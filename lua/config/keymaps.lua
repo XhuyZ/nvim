7c7f6fb5168210b5a64598327dfb1db2da11310a
XhuyZ <huylovengoc35@gmail.com>
2025-03-27 21:58:35 +0700

    add inc-rename plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<leader>rn", ":IncRename ")
