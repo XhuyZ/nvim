63007764143db11504bd6722927be57d75c2136e
xhuyz <huylovengoc35@gmail.com>
2025-04-17 09:41:33 +0700

    configure gemini and nvim-dap plugins

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,4 +1,27 @@
-return
-{
-  'mfussenegger/nvim-dap'
+return {
+	{
+		"mfussenegger/nvim-dap",
+		event = "User BaseFile",
+		config = function()
+			local dap = require("dap")
+
+			-- C#
+			dap.adapters.coreclr = {
+				type = "executable",
+				command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+				args = { "--interpreter=vscode" },
+			}
+			dap.configurations.cs = {
+				{
+					type = "coreclr",
+					name = "launch - netcoredbg",
+					request = "launch",
+					program = function() -- Ask the user what executable wants to debug
+						return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/Program.dll", "file")
+					end,
+				},
+			}
+		end,
+	},
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 }
