a099d844a2188106008f0a50dbaedbcc519ca3d6
xhuyz <huylovengoc35@gmail.com>
2025-04-18 09:43:46 +0700

    finalize the nvim-dap plugin

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -5,23 +5,70 @@ return {
 		config = function()
 			local dap = require("dap")
 
-			-- C#
+			-- C# adapter
 			dap.adapters.coreclr = {
 				type = "executable",
-				command = vim.fn.stdpath("data") .. "/home/xhuyz/.local/share/nvim/mason/bin/",
-				args = { "-m", "netcoredbg.adapter" },
+				command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+				args = { "--interpreter=vscode" },
 			}
+
+			-- C# configurations
 			dap.configurations.cs = {
 				{
 					type = "coreclr",
 					name = "launch - netcoredbg",
 					request = "launch",
-					program = function() -- Ask the user what executable wants to debug
-						return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/Program.dll", "file")
+					program = function()
+						return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/net7.0/Program.dll", "file")
 					end,
 				},
 			}
+
+			-- Keybindings
+			vim.keymap.set("n", "<F5>", function()
+				dap.continue()
+			end, { desc = "DAP Continue" })
+			vim.keymap.set("n", "<F9>", function()
+				dap.toggle_breakpoint()
+			end, { desc = "Toggle Breakpoint" })
+			vim.keymap.set("n", "<F10>", function()
+				dap.step_over()
+			end, { desc = "Step Over" })
+			vim.keymap.set("n", "<F11>", function()
+				dap.step_into()
+			end, { desc = "Step Into" })
+			vim.keymap.set("n", "<F12>", function()
+				dap.step_out()
+			end, { desc = "Step Out" })
+			vim.keymap.set("n", "<leader>db", function()
+				dap.set_breakpoint()
+			end, { desc = "Set Breakpoint" })
+			vim.keymap.set("n", "<leader>dr", function()
+				dap.repl.open()
+			end, { desc = "Open DAP REPL" })
 		end,
 	},
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dapui = require("dapui")
+			local dap = require("dap")
+
+			dapui.setup()
+
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
 }
